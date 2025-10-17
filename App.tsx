c9ad123f1014698c91d4c2a9696c70b74ad26c07
Emilio Venegas <emiliovenegas10@gmail.com>
2025-10-17 16:54:38 -0400

    Fixed Second monitor scale

--- App.tsx
+++ App.tsx
@@ -15,7 +15,7 @@ import { saveAs } from 'file-saver';
 
 // --- ICONS  ---
 const UploadIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
+    <svg xmlns="http://www.w.org/2000/svg" className="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
         <path fillRule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM6.293 6.707a1 1 0 010-1.414l3-3a1 1 0 011.414 0l3 3a1 1 0 01-1.414 1.414L11 5.414V13a1 1 0 11-2 0V5.414L7.707 6.707a1 1 0 01-1.414 0z" clipRule="evenodd" />
     </svg>
 );
@@ -450,7 +450,7 @@ const ProjectionView: React.FC = () => {
                 </div>
             )}
             {!showCalibration && imageUrl ? (
-                <img src={imageUrl} alt="Projection" className="max-w-full max-h-full" style={{ transform, filter }} />
+                <img src={imageUrl} alt="Projection" style={{ height: '100vh', transform, filter }} />
             ) : !showCalibration && (
                 <div className="text-white text-2xl">Waiting for connection...</div>
             )}
